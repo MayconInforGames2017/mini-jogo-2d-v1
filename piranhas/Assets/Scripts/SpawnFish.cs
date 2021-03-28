c004e7d58d7060cbc0c9afe9ee6784cf94eae766
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-03-28 20:07:09 -0300

    tentado resolver bugs no game

--- piranhas/Assets/Scripts/SpawnFish.cs
+++ piranhas/Assets/Scripts/SpawnFish.cs
@@ -8,18 +8,42 @@ public class SpawnFish : MonoBehaviour
 
     public float spawnRate = 2f;
 
+    private bool isPositionBoat = false;
+
+    private Transform boatTransform;
+
     // Start is called before the first frame update
     void Start()
     {
+        boatTransform = GameObject.FindGameObjectWithTag("Boat").transform;
+
         // A cada 2 segundos esse metodo é acionado
         InvokeRepeating("Spawn", spawnRate, spawnRate);
     }
 
     private void Spawn()
     {
+        isPositionBoat = !isPositionBoat;
+
+        Vector3 fishPosition;
+
+        if (isPositionBoat)
+        {
+            fishPosition = new Vector3(transform.position.x,
+                                         boatTransform.position.y,
+                                         transform.position.z);
+        }
+
+        else
+        {
+            fishPosition = new Vector3(transform.position.x,
+                                        Random.Range(-4, -3),
+                                        transform.position.z);
+        }
+
         var fishTransform = Instantiate(fishPrefab) as Transform;
 
-        fishTransform.position = fishTransform.position;
+        fishTransform.position = fishPosition;
     }
 
 }
