0f4a204089e24cbdce38b19b03e2f15d037720e0
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-03-28 13:38:50 -0300

    atualizando camera, scripts do inimigo e os objetos na tela do game

---
+++ piranhas/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Velocidade do inimigo
+    public Vector2 speed = new Vector2 (5, 5);
+
+    // Movimento do inimigo
+    private Vector2 movement;
+
+    private Vector2 direction = new Vector2 (-1, 0);
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimentacao pela direita
+        movement = new Vector2(
+            direction.x * speed.x,
+            direction.y * speed.y);
+    }
+
+    void FixedUpdate()
+    {
+        // Movimento do objeto
+        GetComponent<Rigidbody2D>().velocity = movement;
+    }
+
+}
