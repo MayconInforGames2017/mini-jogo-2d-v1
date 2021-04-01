8785330e7f75c694ebef9222ac6533adf93011cd
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-01 11:34:58 -0300

    implementando o código

--- piranhas/Assets/Scripts/CountDownScript.cs
+++ piranhas/Assets/Scripts/CountDownScript.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountDownScript : MonoBehaviour
 {
+    public Text displayContagem;
+
+    public float contagem = 30.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,14 @@ public class CountDownScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (contagem > 0)
+        {
+            contagem -= Time.deltaTime;
+            displayContagem.text = contagem.ToString();
+        }
+        else
+        {
+            displayContagem.text = "Game Over!!";
+        }
     }
 }
