c9f1862b9a4ace7a7df6803f9ad050ac7e716a6e
Maycon R Santana <maycon_gus2009@hotmail.com>
2021-04-01 16:00:28 -0300

    excluido objeto player

--- piranhas/Assets/Scripts/PlayerMovement.cs
+++ piranhas/Assets/Scripts/PlayerMovement.cs
@@ -5,33 +5,15 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-  
-    public float speed = 10f;
 
-    Vector2 lastClickedPos;
-
-    bool moving;
 
     private void update()
     {
-
-
-        if (Input.GetMouseButtonDown(0))
+    
+        foreach (Touch toque in Input.touches)
         {
-            lastClickedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            moving = true;
-            Debug.Log("Cliquei");
+            Debug.Log("ID: " + toque.fingerId);
         }
 
-        if (moving && (Vector2)transform.position != lastClickedPos)
-        {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, lastClickedPos, step);
-            Debug.Log("Cliquei");
-        }
-        else
-        {
-            moving = false;
-        }
     }
 }
